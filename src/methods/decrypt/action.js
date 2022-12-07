5d742913b38a6fc7dc18074ec4f32482db5a5bb5
GlawinFonceca <glawin.f@juegostudioz.com>
2022-12-07 14:08:56 +0530

    FIX:decrypt action

--- src/methods/decrypt/action.js
+++ src/methods/decrypt/action.js
@@ -6,9 +6,8 @@ class DecryptAction extends baseAction {
       let [decryptLib] = AutoLoad.loadLibray("helperLib", ["decode"]);
       let { data } = this;
       let decodedData = decryptLib.decryptText(data);
-      console.log(decodedData);
       this.setResponse("SUCCESS");
-      return { ...this };
+      return { decodedData };
     } catch (e) {
       console.log("Error decrypt =>", e);
     }
